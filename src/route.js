d93ed7dd250ddc35be0788bd7b3c96028163e6b9
Nate Hunzaker <nate.hunzaker@gmail.com>
2016-01-04 13:45:30 -0500

    Do not reply on Function.prototype.bind

--- src/route.js
+++ src/route.js
@@ -7,7 +7,7 @@ module.exports = function route(API, routes={}) {
     // For each endpoint (create, read, update, delete...)
     return remap(resource, function(options) {
       let config  = Object.assign({}, API.config, options)
-      let request = API.ajax.bind(null, config)
+      let request = (overrides) => API.ajax(config, overrides)
 
       // Make the config for this route available under 'config'
       request.config = config
