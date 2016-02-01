124483998bb2090ab730a015a5acf40973a367f6
Nate Hunzaker <nate.hunzaker@gmail.com>
2016-02-01 16:09:47 -0500

    Revert defaults

--- src/prepare.js
+++ src/prepare.js
@@ -2,9 +2,22 @@
  * Prepare request configuration, deeply merging some specific keys
  */
 
-var DEFAULTS = require('./defaults')
 var assign   = require('./assign')
 var toArray  = require('./toArray')
+
+var DEFAULTS = {
+  baseURL    : '/',
+  body       : undefined,
+  params     : undefined,
+  headers    : {},
+  method     : 'GET',
+  path       : '',
+  query      : {},
+  type       : 'application/json',
+  beforeSend : function(ajax) { return ajax },
+  onResponse : function(response) { return response.body },
+  onError    : function(error) { return error }
+}
 
 module.exports = function prepare (/* options list */) {
   var options = [ DEFAULTS ].concat(toArray(arguments)).filter(i => i != undefined)
