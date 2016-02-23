8de444675c9b6720e987b295e6b80b842ec56f94
Nate Hunzaker <nate.hunzaker@gmail.com>
2016-02-23 07:51:37 -0500

    Remove promises. Add warning about missing Promise support.

--- src/mock.js
+++ src/mock.js
@@ -1,7 +1,3 @@
-var Promise = require('promise')
-
 module.exports = function (options) {
-  var reply = typeof options.mock === 'function' ? options.mock(options) : options.mock
-
-  return Promise.resolve(reply)
+  return typeof options.mock === 'function' ? options.mock(options) : options.mock
 }
